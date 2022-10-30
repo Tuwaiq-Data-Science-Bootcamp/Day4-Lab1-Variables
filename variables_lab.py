ac0b724a51a2f7033e97eee011baa4abbae5dba3
Nada Alosaimi <90303427+NadaAlosaimi@users.noreply.github.com>
2022-10-30 08:27:55 +0300

    Variables Lab

---
+++ variables_lab.py
@@ -0,0 +1,12 @@
+# Define a variable that has a value of your favourite season.
+fav_season : str = 'Winter'
+
+# Define “x“ variable has an integer value and variable “y” has a float value.
+x : int = 7
+y : float = 7.19
+
+# Assign One Value to Multiple Variables.
+nadas_major, jumanas_major, reemas_major : str = 'Data science and AI'
+
+# Assign Many Values to Multiple Variables.
+shathas_major, modhi_major, fay_major : str = 'IT', 'SWE', 'IS'
